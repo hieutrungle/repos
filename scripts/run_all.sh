aaf6211d4f7f118b87512fd43b21997f1a1da8c0
hieule <hieult1996@gmail.com>
2026-05-01 14:50:20 -0500

    move reflector

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,2 +1,7 @@
-python scripts/run_all_methods_ap_sweep.py --methods all --config configs/run_experiments_cuda_hrbb.json --ap_min 1 --ap_max 3 --seeds 41 42 --output_dir "tmp_comparison_results/hrbb_experiments"
-python scripts/run_all_methods_ap_sweep.py --methods all --config configs/run_experiments_cuda_office.json --ap_min 1 --ap_max 3 --seeds 41 42 --output_dir "tmp_comparison_results/office_experiments"
+# python scripts/run_all_methods_ap_sweep.py --methods all --config configs/run_experiments_cuda_hrbb.json --ap_min 1 --ap_max 6 --seeds 41 42 43 44 --output_dir "tmp_comparison_results1/hrbb_experiments"
+# python scripts/run_all_methods_ap_sweep.py --methods all --config configs/run_experiments_cuda_office.json --ap_min 1 --ap_max 6 --seeds 41 42 43 44 --output_dir "tmp_comparison_results1/office_experiments"
+
+python scripts/run_all_methods_ap_sweep.py --methods all --config configs/run_experiments_cuda_office1.json --ap_min 1 --ap_max 6 --seeds 41 42 43 44 --output_dir "tmp_reflector/office_experiments1"
+python scripts/run_all_methods_ap_sweep.py --methods all --config configs/run_experiments_cuda_office2.json --ap_min 1 --ap_max 6 --seeds 41 42 43 44 --output_dir "tmp_reflector/office_experiments2"
+python scripts/run_all_methods_ap_sweep.py --methods all --config configs/run_experiments_cuda_office3.json --ap_min 1 --ap_max 6 --seeds 41 42 43 44 --output_dir "tmp_reflector/office_experiments3"
+python scripts/run_all_methods_ap_sweep.py --methods all --config configs/run_experiments_cuda_office4.json --ap_min 1 --ap_max 6 --seeds 41 42 43 44 --output_dir "tmp_reflector/office_experiments4"
